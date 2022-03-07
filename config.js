26f891cf928728adfd97ec1d1d9db0daced16c49
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2022-03-07 20:37:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const nameGirl = '';
+const nameGirl = 'Thanh Thảo yêu dấu <3';
 const giftUrl = 'https://www.facebook.com/thanhthao803';
-const eventName = 'Chúc mừng sinh nhật Thanh Thảo yêu dấu <3';
-const titleCard = 'Gửi Thảo';
+const eventName = 'Chúc mừng sinh nhật bạn ';
+const titleCard = 'Gửi bạn Thảo';
 const contentCard = 'Chúc Thanh Thảo sinh nhật 3 tủi lần thứ 18 tràn ngập niềm vui và những nụ cười nhé. Mong điều tốt đẹp sẽ đến với Thảo trong hôm nay và cả những ngày sau nha. You’ll never be alone, I’ll be with you from dusk till dawn.';
 
 const giftImage = 'a1.jpg';
